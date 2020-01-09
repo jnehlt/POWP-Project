424fb44f0ee1d800f8040f754fc5c6fd95835b1c
Paweł Krystian <pk.hapti@gmail.com>
2020-01-09 16:10:09 +0100

    Added mocked getAllProfiles

--- src/main/java/pl/com/abstractcompany/powpproject/model/Profile.java
+++ src/main/java/pl/com/abstractcompany/powpproject/model/Profile.java
@@ -1,7 +1,14 @@
 package pl.com.abstractcompany.powpproject.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Profile {
+    private String name;
+    private String surname;
+    private String position;
+    private String email;
+    private String phone;
 }
