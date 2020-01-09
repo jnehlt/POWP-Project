1e336267cd53d4509fbbc47bdc5c9a7731f8acb1
Paweł Krystian <pk.hapti@gmail.com>
2020-01-09 16:17:31 +0100

    added id for profiles

--- src/main/java/pl/com/abstractcompany/powpproject/model/Profile.java
+++ src/main/java/pl/com/abstractcompany/powpproject/model/Profile.java
@@ -3,9 +3,16 @@ package pl.com.abstractcompany.powpproject.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @AllArgsConstructor
 public class Profile {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String surname;
     private String position;
--- src/main/java/pl/com/abstractcompany/powpproject/repository/ProfilesRepository.java
+++ src/main/java/pl/com/abstractcompany/powpproject/repository/ProfilesRepository.java
@@ -9,9 +9,9 @@ import java.util.stream.Stream;
 //it's a mocked repository. We dont want to use any db or whatsoever - there is no need of that
 public class ProfilesRepository {
     public Stream<Profile> getAllProfiles(){
-        var profile1 = new Profile("Jan", "Kowalski", "Szef wszystkich szefów", "jk@powp.com", "123456789");
-        var profile2 = new Profile("Igrek", "Iksiński", "Konserwator powieszchni płaskich", "ii@powp.com", "987321456");
-        var profile3 = new Profile("John", "Doe", "Manager", "jd@powp.com", "999888777");
+        var profile1 = new Profile(1L,"Jan", "Kowalski", "Szef wszystkich szefów", "jk@powp.com", "123456789");
+        var profile2 = new Profile(2L,"Igrek", "Iksiński", "Konserwator powieszchni płaskich", "ii@powp.com", "987321456");
+        var profile3 = new Profile(3L, "John", "Doe", "Manager", "jd@powp.com", "999888777");
 
         return Stream.of(profile1, profile2, profile3);
     }
